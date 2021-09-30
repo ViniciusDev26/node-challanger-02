017b4e82c732c205c52eede3d7b7ee42d260ebd3
Carlos Vinicius <vinicius26092000@gmail.com>
2021-09-30 17:27:03 -0300

    challenger complete

--- src/index.js
+++ src/index.js
@@ -10,19 +10,63 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if(!user) 
+    return response.status(404).json();
+
+  request.user = user;
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  const userHavePermission = user.pro || user.todos.length < 10; 
+
+  if(userHavePermission){
+    return next();
+  }
+
+  return response.status(403).json({error: "user not have permission"})
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const { username } = request.headers;
+
+  if(!validate(id)){
+    return response.status(400).json();
+  }
+
+  const user = users.find((user) => user.username === username);
+  if(!user){
+    return response.status(404).json();
+  }
+
+  const todo = user.todos.find(task => task.id === id);
+  if(!todo) {
+    return response.status(404).json();
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if(!user) 
+    return response.status(404).json();
+
+  request.user = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
